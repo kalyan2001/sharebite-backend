65d5f17e9bad4a8ef6e647781629ef7be4d571ca
kalyan2001 <95684291+kalyan2001@users.noreply.github.com>
2025-12-08 19:18:15 -0500

    Updated Firebase admin config for Render deployment

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,11 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
 
-const serviceAccount = JSON.parse(
-  readFileSync("./serviceAccountKey.json", "utf8")
-);
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
